45e6c2ebe4ca4ed5723c867f53d651fab910fb3c
CompaxChris <christian@compax.no>
2021-10-18 15:15:41 +0200

    Basic code working for the test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
+float touchVal = 43;
+int touchValRaw = 43;
+float triggerVal = 53.3;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  touchValRaw = touchRead(32);
+
+  Serial.print(touchVal);
+  Serial.print("  ");
+  Serial.print(touchValRaw);
+  Serial.print("  ");
+  if(touchVal < triggerVal) Serial.print("WATER!");
+  Serial.println("  ");
+
+  if(touchValRaw < touchVal - 10) touchValRaw = touchVal; //sletter hvis det er for mye avvik
+  touchVal = touchVal * 0.9 + touchValRaw * 0.1;
+  delay(50);
 }
