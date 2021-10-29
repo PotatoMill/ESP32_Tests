cada6972d4226605cb321479f1e56be98c7c9384
CompaxChris <christian@compax.no>
2021-10-29 13:27:54 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,63 @@
 #include <Arduino.h>
-float touchVal = 43;
-int touchValRaw = 43;
-float triggerVal = 53.3;
+float touchVal = 0;
+int touchValRaw = 0;
+float triggerVal1 = 42;
+float triggerVal2 = 41.5;
+float triggerVal3 = 41;
+#define LED_PIN_1 26
+#define LED_PIN_2 25
+#define LED_PIN_3 14
 
-void setup() {
+
+void setup()
+{
   Serial.begin(115200);
+  pinMode(LED_PIN_1, OUTPUT);
+  pinMode(LED_PIN_2, OUTPUT);
+  pinMode(LED_PIN_3, OUTPUT);
+  digitalWrite(LED_PIN_1, HIGH);
+  digitalWrite(LED_PIN_2, HIGH);
+  digitalWrite(LED_PIN_3, HIGH); // følger rekkefølge på pinnene
+  delay(500);
 }
 
-void loop() {
+void loop()
+{
   touchValRaw = touchRead(32);
 
   Serial.print(touchVal);
   Serial.print("  ");
   Serial.print(touchValRaw);
   Serial.print("  ");
-  if(touchVal < triggerVal) Serial.print("WATER!");
+  if (touchVal < triggerVal1)
+  {
+    Serial.print("WATER!");
+    digitalWrite(LED_PIN_1, HIGH);
+  }
+  else
+    digitalWrite(LED_PIN_1, LOW);
+  Serial.print("  ");
+
+  if (touchVal < triggerVal2)
+  {
+    Serial.print("WATER!");
+    digitalWrite(LED_PIN_2, HIGH);
+  }
+  else
+    digitalWrite(LED_PIN_2, LOW);
+  Serial.print("  ");
+
+  if (touchVal < triggerVal3)
+  {
+    Serial.print("WATER!");
+    digitalWrite(LED_PIN_3, HIGH);
+  }
+  else
+    digitalWrite(LED_PIN_3, LOW);
   Serial.println("  ");
 
-  if(touchValRaw < touchVal - 10) touchValRaw = touchVal; //sletter hvis det er for mye avvik
+  if (touchValRaw < touchVal - 10)
+    touchValRaw = touchVal; // sletter hvis det er for mye avvik
   touchVal = touchVal * 0.9 + touchValRaw * 0.1;
   delay(50);
 }
