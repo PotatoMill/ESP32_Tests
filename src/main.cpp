6100c603e66cbe0fcd51abd5c3c7cbfc90ce7ba8
CompaxChris <christian@compax.no>
2021-11-16 09:48:01 +0100

    If wørks!
    tested up to 210khz

--- src/main.cpp
+++ src/main.cpp
@@ -24,12 +24,12 @@
 const int pwmFanPin1 = 21;  // 21 corresponds to GPIO21?
 
 // setting PWM properties
-const int freq = 1000;
+const int freq = 50200;
 const int ledChannel = 0;
-const int resolution = 8;
+const int resolution = 4;
 
-int lower_limit = -10000;
-int upper_limit =  20;
+int lower_limit = -35000;
+int upper_limit =  500;
 int threshold_value = -50;
 
 pcnt_isr_handle_t user_isr_handle = NULL; //user's ISR service handle
@@ -47,9 +47,9 @@ typedef struct {
  * and pass this information together with the event type
  * and timestamp to the main program using a queue.
  */
-static void IRAM_ATTR pcnt_intr_handler(void *arg)
-{
-}
+//static void IRAM_ATTR pcnt_intr_handler(void *arg)
+//{
+//}
 
 
 /* Initialize PCNT functions for one channel:
@@ -82,7 +82,7 @@ void pcnt_init_channel(pcnt_unit_t PCNT_UNIT,int PCNT_INPUT_SIG_IO , int PCNT_IN
     /* Initialize PCNT unit */
     pcnt_unit_config(&pcnt_config);
     /* Configure and enable the input filter */
-    pcnt_set_filter_value(PCNT_UNIT, 100);
+    pcnt_set_filter_value(PCNT_UNIT, 50);
     pcnt_filter_enable(PCNT_UNIT);
 
     /* Set threshold 0 and 1 values and enable events to watch */
@@ -99,7 +99,7 @@ void pcnt_init_channel(pcnt_unit_t PCNT_UNIT,int PCNT_INPUT_SIG_IO , int PCNT_IN
     pcnt_counter_pause(PCNT_UNIT);
     pcnt_counter_clear(PCNT_UNIT);
     /* Register ISR handler and enable interrupts for PCNT unit */
-    pcnt_isr_register(pcnt_intr_handler, NULL, 0, &user_isr_handle);
+    //pcnt_isr_register(pcnt_intr_handler, NULL, 0, &user_isr_handle);
     pcnt_intr_enable(PCNT_UNIT);
 
     /* Everything is set up, now go to counting */
@@ -107,10 +107,14 @@ void pcnt_init_channel(pcnt_unit_t PCNT_UNIT,int PCNT_INPUT_SIG_IO , int PCNT_IN
 
 }
 
+float calculate_frequency(int number_of_pulses, int start_time, int stop_time){
+  return 1000.0*number_of_pulses/(stop_time-start_time);
+}
+
 void setup() {
   Serial.begin(115200);
 
-  ledcSetup(ledChannel,freq,resolution);
+  ledcSetup(ledChannel,210000,resolution);
   
   // attach the channel to the GPIO to be controlled
   ledcAttachPin(21, ledChannel);
@@ -125,10 +129,29 @@ void loop()
 
   Serial.println("starting infinite loop");
 
+  int loops_for_print = 0;
+  unsigned long last_time_Stamp = 0;
   int16_t count = 0;
+  float frequency = 0;
   for(;;){
     pcnt_get_counter_value(PCNT_UNIT_0, &count);
-    Serial.printf("\n Counter %d", count);
-    delay(200);
+    if(count<= -30000) {
+      frequency = calculate_frequency(count, micros(), last_time_Stamp);
+      last_time_Stamp = micros();
+      pcnt_counter_clear(PCNT_UNIT_0);
+    }
+    if(loops_for_print >= (100000)){
+    Serial.printf("\n Counter %0.3f kHz", frequency);
+    loops_for_print = 0;
+    }
+    loops_for_print++;
+
+    
+
+  }
+    if(user_isr_handle) {
+    //Free the ISR service handle.
+    esp_intr_free(user_isr_handle);
+    user_isr_handle = NULL;
   }
 }
