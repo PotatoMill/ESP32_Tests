1bf2e8c9086cd3ef0ccd12719f7f5f5559749b84
CompaxChris <christian@compax.no>
2021-11-16 16:23:56 +0100

    Clening up code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,3 @@
-
-/* Pulse counter module - Example
-   For other examples please check:
-   https://github.com/espressif/esp-idf/tree/master/examples
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <Arduino.h>
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
@@ -20,47 +11,9 @@
 #include "esp_attr.h"
 #include "esp_log.h"
 
-// the number of the LED pin
-const int pwmFanPin1 = 21;  // 21 corresponds to GPIO21?
-
-// setting PWM properties
-const int freq = 50200;
-const int ledChannel = 0;
-const int resolution = 4;
-
-int lower_limit = -35000;
+int lower_limit = -35000; //int is 16 bit unsigned wach out for overflow
 int upper_limit =  500;
-int threshold_value = -50;
 
-pcnt_isr_handle_t user_isr_handle = NULL; //user's ISR service handle
-
-/* A sample structure to pass events from the PCNT
- * interrupt handler to the main program.
- */
-typedef struct {
-    int unit;  // the PCNT unit that originated an interrupt
-    uint32_t status; // information on the event type that caused the interrupt
-    unsigned long timeStamp; // The time the event occured
-} pcnt_evt_t;
-
-/* Decode what PCNT's unit originated an interrupt
- * and pass this information together with the event type
- * and timestamp to the main program using a queue.
- */
-//static void IRAM_ATTR pcnt_intr_handler(void *arg)
-//{
-//}
-
-
-/* Initialize PCNT functions for one channel:
- *  - configure and initialize PCNT with pos-edge counting 
- *  - set up the input filter
- *  - set up the counter events to watch
- * Variables:
- * UNIT - Pulse Counter #, INPUT_SIG - Signal Input Pin, INPUT_CTRL - Control Input Pin,
- * Channel - Unit input channel, H_LIM - High Limit, L_LIM - Low Limit,
- * THRESH1 - configurable limit 1, THRESH0 - configurable limit 2, 
- */
 void pcnt_init_channel(pcnt_unit_t PCNT_UNIT,int PCNT_INPUT_SIG_IO , int PCNT_INPUT_CTRL_IO = PCNT_PIN_NOT_USED,pcnt_channel_t PCNT_CHANNEL = PCNT_CHANNEL_0, int PCNT_H_LIM_VAL = 19, int PCNT_L_LIM_VAL = -20, int PCNT_THRESH1_VAL = 50, int PCNT_THRESH0_VAL = -50 ) {
     /* Prepare configuration for the PCNT unit */
     pcnt_config_t pcnt_config; 
@@ -69,27 +22,18 @@ void pcnt_init_channel(pcnt_unit_t PCNT_UNIT,int PCNT_INPUT_SIG_IO , int PCNT_IN
         pcnt_config.ctrl_gpio_num = PCNT_INPUT_CTRL_IO;
         pcnt_config.channel = PCNT_CHANNEL;
         pcnt_config.unit = PCNT_UNIT;
-        // What to do on the positive / negative edge of pulse input?
         pcnt_config.pos_mode = PCNT_COUNT_INC;   // Count up on the positive edge
         pcnt_config.neg_mode = PCNT_COUNT_DIS;   // Keep the counter value on the negative edge
-        // What to do when control input is low or high?
         pcnt_config.lctrl_mode = PCNT_MODE_REVERSE; // Reverse counting direction if low
         pcnt_config.hctrl_mode = PCNT_MODE_KEEP;    // Keep the primary counter mode if high
-        // Set the maximum and minimum limit values to watch
-        pcnt_config.counter_h_lim = PCNT_H_LIM_VAL;
-        pcnt_config.counter_l_lim = PCNT_L_LIM_VAL;
+        pcnt_config.counter_h_lim = PCNT_H_LIM_VAL; //max value before resetting
+        pcnt_config.counter_l_lim = PCNT_L_LIM_VAL; //min value before resetting
     
     /* Initialize PCNT unit */
     pcnt_unit_config(&pcnt_config);
-    /* Configure and enable the input filter */
-    pcnt_set_filter_value(PCNT_UNIT, 50);
+    pcnt_set_filter_value(PCNT_UNIT, 50); //input filter
     pcnt_filter_enable(PCNT_UNIT);
 
-    /* Set threshold 0 and 1 values and enable events to watch */
-    // pcnt_set_event_value(PCNT_UNIT, PCNT_EVT_THRES_1, PCNT_THRESH1_VAL);
-    // pcnt_event_enable(PCNT_UNIT, PCNT_EVT_THRES_1);
-    // pcnt_set_event_value(PCNT_UNIT, PCNT_EVT_THRES_0, PCNT_THRESH0_VAL);
-    // pcnt_event_enable(PCNT_UNIT, PCNT_EVT_THRES_0);
     /* Enable events on zero, maximum and minimum limit values */
     pcnt_event_enable(PCNT_UNIT, PCNT_EVT_ZERO);
     pcnt_event_enable(PCNT_UNIT, PCNT_EVT_H_LIM);
@@ -102,7 +46,6 @@ void pcnt_init_channel(pcnt_unit_t PCNT_UNIT,int PCNT_INPUT_SIG_IO , int PCNT_IN
     //pcnt_isr_register(pcnt_intr_handler, NULL, 0, &user_isr_handle);
     pcnt_intr_enable(PCNT_UNIT);
 
-    /* Everything is set up, now go to counting */
     pcnt_counter_resume(PCNT_UNIT);
 
 }
@@ -114,12 +57,6 @@ float calculate_frequency(int number_of_pulses, int start_time, int stop_time){
 void setup() {
   Serial.begin(115200);
 
-  ledcSetup(ledChannel,210000,resolution);
-  
-  // attach the channel to the GPIO to be controlled
-  ledcAttachPin(21, ledChannel);
-  ledcWrite(ledChannel, 10); // 1Hz PWM with duty cycle of 10/255
-  Serial.println("setup complete");
 }
 
 void loop()
@@ -152,10 +89,5 @@ void loop()
 
     
 
-  }
-    if(user_isr_handle) {
-    //Free the ISR service handle.
-    esp_intr_free(user_isr_handle);
-    user_isr_handle = NULL;
   }
 }
