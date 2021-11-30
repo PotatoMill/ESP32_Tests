2145ab0231c3e7a161ae122f2e3feed6fdc73046
CompaxChris <christian@compax.no>
2021-11-30 15:13:09 +0100

    Basic men funker ikke skikkelig

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,47 @@
 #include <Arduino.h>
+#include "AsyncUDP.h"
+#include <WiFi.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const char * ssid = "CompaxNet";
+const char * password = "Bosmannen";
+
+AsyncUDP udp;
+
+void setup()
+{
+    Serial.begin(115200);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.println("WiFi Failed");
+        while(1) {
+            delay(1000);
+        }
+    }
+    Serial.println(WiFi.localIP());
+
+    //The server is receiving on port 6309
+    //if(udp.listen(6309)) {
+    /* if(udp.connect(IPAddress(255,255,255,255), 6309)) {
+        Serial.println("UDP connected");
+        udp.onPacket([](AsyncUDPPacket packet) {
+            Serial.print(", From: ");
+            Serial.print(packet.remoteIP());
+            Serial.print(", Data: ");
+            Serial.write(packet.data(), packet.length());
+            Serial.println();
+            //reply to the client
+            packet.printf("Got %u bytes of data", packet.length());
+        });
+        //Send unicast
+        udp.print("Hello Server!");
+    } */
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    delay(1000);
+    //Send broadcast on port 1234
+    Serial.print(".");
+    udp.broadcastTo("test", 1234);
 }
