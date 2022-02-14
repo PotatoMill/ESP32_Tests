08e6c5057775801da1e7c1fc1d21282acab088bb
CompaxChris <christian@compax.no>
2022-02-14 12:08:11 +0100

    Fungerende grunleggende PWM kode

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
+//simple code that fades a led up and down using pwm
+
 #include <Arduino.h>
 
+#define PWM_CHANNEL0 0
+#define PWM_CHANNEL0_PIN 5
+#define PWM_CHANNEL0_RESOLUTION 8
+#define PWM_CHANNEL0_FREQUENCY 10000
+
+int pwm_channel0_duty_cycle = 0;
+bool goingUp = true;
+
 void setup() {
-  // put your setup code here, to run once:
+  ledcAttachPin(PWM_CHANNEL0_PIN, PWM_CHANNEL0);
+  ledcSetup(PWM_CHANNEL0, PWM_CHANNEL0_FREQUENCY, PWM_CHANNEL0_RESOLUTION);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  ledcWrite(PWM_CHANNEL0, pwm_channel0_duty_cycle);
+
+  if(goingUp) pwm_channel0_duty_cycle++;
+  else pwm_channel0_duty_cycle--;
+
+  if (pwm_channel0_duty_cycle>=255) goingUp=false;
+  if (pwm_channel0_duty_cycle<=0) goingUp=true;
+
+  delay(1);
 }
