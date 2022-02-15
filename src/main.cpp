e35af4568a4c406ae588e35f087978dd5745ae4b
CompaxChris <christian@compax.no>
2022-02-15 12:18:34 +0100

    Basic code set up
    This code sets up a server on the esp32 and lisens for clients. It wil then print data from the client to the serial monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,45 @@
 #include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+const char* ssid     = "CompaxNet";
+const char* password = "Bosmannen";
+/* create a server and listen on port 8088 */
+WiFiServer server(8088);
+void setup()
+{
+    Serial.begin(115200);
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    /* connecting to WiFi */
+    WiFi.begin(ssid, password);
+    /*wait until ESP32 connect to WiFi*/
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected with IP address: ");
+    Serial.println(WiFi.localIP());
+    /* start Server */
+    server.begin();
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+    /* listen for client */
+    WiFiClient client = server.available(); //dette gir muligheten til å ha flere klienter
+    uint8_t data[30]; 
+    if (client) {                   
+      Serial.println("new client");         
+      /* check client is connected */           
+      while (client.connected()) {          
+          if (client.available()) {
+              int len = client.read(data, 30);
+              if(len < 30){
+                  data[len] = '\0';  
+              }else {
+                  data[30] = '\0';
+              }    
+              Serial.print("client sent: ");            
+              Serial.println((char *)data); 
+          }
+      } 
+    }
 }
